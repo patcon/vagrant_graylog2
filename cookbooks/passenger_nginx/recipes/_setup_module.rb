b9782b814d584a5f3406bab096573cf2ae432810
Marius Sturm <marius.sturm@xing.com>
2012-04-12 12:45:45 +0200

    fix nginx attributes name

--- cookbooks/passenger_nginx/recipes/_setup_module.rb
+++ cookbooks/passenger_nginx/recipes/_setup_module.rb
@@ -17,7 +17,7 @@ bash "install passenger nginx module" do
   code <<-EOH
     passenger-install-nginx-module --auto \
       --nginx-source-dir="#{Chef::Config[:file_cache_path]}/nginx-#{node['nginx']['version']}" \
-      --prefix="#{node['nginx']['install_path']}" \
+      --prefix="#{node['nginx']['source']['prefix']}" \
       --extra-configure-flags="#{nginx_configure_flags.join(' ')}"
   EOH
   not_if "#{node['nginx']['install_path']}/sbin/nginx -V 2>&1 | grep passenger"
